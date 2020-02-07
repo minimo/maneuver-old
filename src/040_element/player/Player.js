79ab4314c49e2f35983cea618c475859a4f48472
yoshifumi.fujimoto <yoshifumi.fujimoto@alphacode.co.jp>
2020-02-07 20:39:13 +0900

    update

--- src/040_element/player/Player.js
+++ src/040_element/player/Player.js
@@ -21,40 +21,6 @@ phina.namespace(function() {
     },
 
     update: function() {
-      if (this.time % 3 == 0) {
-        var ct = phina_app.controller;
-        if (ct.left) {
-          this.direction++;
-          if (this.direction > 15) this.direction = 0;
-        } else if (ct.right) {
-          this.direction--;
-          if (this.direction < 0) this.direction = 15;
-        }
-        this.sprite.setFrameIndex(this.direction);
-        if (ct.up) {
-          this.speed += 0.1;
-          if (this.speed > 1) this.speed = 1;
-          const rad = (this.direction * 22.5).toRadian();
-          this.vx += -Math.sin(rad) * this.speed;
-          this.vy += -Math.cos(rad) * this.speed;
-          const vec = Vector2(this.vx, this.vy);
-          if (vec.length > 2) {
-            vec.normalize();
-            this.vx = vec.x * 2;
-            this.vy = vec.y * 2;
-          }
-        } else {
-          this.speed *= 0.98;
-        }
-      }
-
-      this.x += this.vx;
-      this.y += this.vy;
-
-      this.vx *= 0.999;
-      this.vy *= 0.999;
-
-      this.time++;
     },
   });
 
--- src/040_element/world/World.js
+++ src/040_element/world/World.js
@@ -6,6 +6,8 @@ phina.namespace(function() {
     init: function(options) {
       this.superInit();
       this.setup();
+
+      this.time = 0;
     },
 
     setup: function() {
@@ -23,14 +25,70 @@ phina.namespace(function() {
       this.player = Player()
         .setPosition(SCREEN_WIDTH_HALF, SCREEN_HEIGHT_HALF)
         .addChildTo(this.mapLayer[LAYER_PLAYER]);
+
+      this.setupMap();
     },
     update: function() {
+      this.controlPlayer();
+
       this.mapBase.x = -this.player.x + SCREEN_WIDTH_HALF;
       this.mapBase.y = -this.player.y + SCREEN_HEIGHT_HALF;
-      console.log(this.mapBase.x, this.mapBase.y)
+
+      this.time++;
     },
     setupMap: function() {
+      for (let i = 0; i < 1000; i++) {
+        RectangleShape({
+          width: Math.randint(50, 200),
+          height: Math.randint(50, 200),
+          fill: 'blue',
+          stroke: '#aaa',
+          strokeWidth: 4,
+          cornerRadius: 0,
+          x: Math.randint(-3000, 3000),
+          y: Math.randint(-3000, 3000),
+        }).addChildTo(this.mapLayer[LAYER_BACKGROUND]);
+      }
     },
+    controlPlayer: function() {
+      const player = this.player;
+      var ct = phina_app.controller;
+      if (this.time % 3 == 0) {
+        if (ct.left) {
+          player.direction++;
+          if (player.direction > 15) player.direction = 0;
+        } else if (ct.right) {
+          player.direction--;
+          if (player.direction < 0) player.direction = 15;
+        }
+        player.sprite.setFrameIndex(player.direction);
+        if (ct.up) {
+          player.speed += 0.1;
+          if (player.speed > 1) player.speed = 1;
+          const rad = (player.direction * 22.5).toRadian();
+          player.vx += -Math.sin(rad) * player.speed;
+          player.vy += -Math.cos(rad) * player.speed;
+          const vec = Vector2(player.vx, player.vy);
+          if (vec.length > 2) {
+            vec.normalize();
+            player.vx = vec.x * 2;
+            player.vy = vec.y * 2;
+          }
+        } else {
+          player.speed *= 0.98;
+        }
+      }
+
+      player.x += player.vx;
+      player.y += player.vy;
+
+      player.vx *= 0.99;
+      player.vy *= 0.99;
+
+      if (!ct.up) {
+        player.vy += 0.098;
+      }
+    }
   });
 
 });
